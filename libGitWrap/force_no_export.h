3954d9d0bf69079fd1e3d9dcf2a1e23fbb28e3fb
Sascha Cunz <Sascha@BabbelBox.org>
2013-01-05 10:28:29 +0100

    Do at least an explanation of this

--- libGitWrap/force_no_export.h
+++ libGitWrap/force_no_export.h
@@ -1,6 +1,15 @@
 
 #pragma once
 
+/*
+ * On MSVC, we do a forced include of this file before any other file can include libgit2's public
+ * common.h file. We then unconditionally undefine the GIT_EXTERN (which by that time includes a
+ * declspec for dll-exports) and redefine it to a simple "extern".
+ *
+ * This leaves only git methods from git2/common.h publicly exported; but those might turn out to
+ * be handy to have, even if no other libgit2 interface shall be accessable.
+ */
+
 #include "libgit2/include/git2/common.h"
 
 #ifdef _MSC_VER
