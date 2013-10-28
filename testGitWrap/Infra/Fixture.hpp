0894c8bdd190404cf5d9ebf584cf8b6e44802c94
Sascha Cunz <sascha@babbelbox.org>
2013-10-28 04:13:58 +0000

    Test infrastructure

---
+++ testGitWrap/Infra/Fixture.hpp
@@ -0,0 +1,46 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2013 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@macgitver.org>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TEST_GW_FIXTURE_HPP
+#define TEST_GW_FIXTURE_HPP
+
+#include <QString>
+#include <QObject>
+
+#include "gtest/gtest.h"
+
+class Fixture : public QObject, public ::testing::Test
+{
+    Q_OBJECT
+public:
+    Fixture();
+    ~Fixture();
+
+public:
+    virtual void SetUp();
+    virtual void TearDown();
+
+public:
+    QString prepareRepo(const char* name);
+
+protected:
+    QString dataDir() const;
+};
+
+#endif
---
+++ testGitWrap/Infra/TempRepo.hpp
@@ -0,0 +1,69 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2013 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@macgitver.org>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TEST_GW_TEMP_REPO_HPP
+#define TEST_GW_TEMP_REPO_HPP
+
+#include <QString>
+
+#include "libGitWrap/Repository.hpp"
+
+class Fixture;
+
+/**
+ * @brief   Copy a Test-Repository and remove it after usage
+ */
+class TempRepo
+{
+public:
+    TempRepo(Fixture* fixture, const char* name);
+    ~TempRepo();
+
+public:
+    operator QString() const
+    {
+        return mTempRepoDir;
+    }
+
+private:
+    QString mTempRepoDir;
+};
+
+/**
+ * @brief   Copy a Test-Repo, remove it after use and keep it opened in RepoMan as long
+ *          as it is in use.
+ */
+class TempRepoOpener
+{
+public:
+    TempRepoOpener(Fixture* fixture, const char* name);
+    ~TempRepoOpener();
+
+public:
+    operator Git::Repository() const
+    {
+        return mRepo;
+    }
+
+private:
+    TempRepo mTempRepo;
+    Git::Repository mRepo;
+};
+
+#endif
