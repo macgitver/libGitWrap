b1ccba1721e0df793edc5c47e4a5a9d844b6b355
Nils Fenner <nilsfenner@web.de>
2014-12-24 12:55:46 +0100

    renamed header file "IRemoteEvents.hpp" -> "IGitEvents.hpp" as it is an interface collection

--- libGitWrap/Events/IGitEvents.hpp
+++ libGitWrap/Events/IGitEvents.hpp
@@ -1,6 +1,6 @@
 /*
  * MacGitver
- * Copyright (C) 2012-2013 Sascha Cunz <sascha@babbelbox.org>
+ * Copyright (C) 2014 Sascha Cunz <sascha@macgitver.org>
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License (Version 2) as published by the Free Software Foundation.
@@ -14,8 +14,8 @@
  *
  */
 
-#ifndef GITWRAP_IREMOTE_EVENTS_HPP
-#define GITWRAP_IREMOTE_EVENTS_HPP
+#ifndef GITWRAP_IGIT_EVENTS_HPP
+#define GITWRAP_IGIT_EVENTS_HPP
 #pragma once
 
 #include "libGitWrap/GitWrap.hpp"
