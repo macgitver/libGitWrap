e8d622749d076cc390e6ce95fa7b4dedec086578
Nils Fenner <nilsfenner@web.de>
2014-12-31 20:00:37 +0100

    cleanup

--- libGitWrap/Operations/CloneOperation.cpp
+++ libGitWrap/Operations/CloneOperation.cpp
@@ -66,7 +66,7 @@ namespace Git
     }
 
     CloneOperation::CloneOperation(QObject* parent)
-        : BaseRemoteOperation(*new Private(this), parent)
+        : BaseRemoteOperation( *new Private(this), parent )
     {
     }
 
